ed1f7120f4fbd676fa64b712677a54f76a025454
Wenter <uuooooq@gmail.com>
2018-06-27 20:38:35 +0800

    测试

--- expressl/myapp/app.js
+++ expressl/myapp/app.js
@@ -6,10 +6,11 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var homeRouter = require('./routes/home');
 
 var app = express();
 
-// view engine setup
+// view engine setupR
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/home', homeRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
