1563bc0e976aba860c965aed79675c34fba05b70
ios-android <uuooooq@qq.com>
2019-12-12 09:55:38 +0800

    删除无用的代码

--- expressl/myapp/config/db.js
+++
@@ -1,6 +0,0 @@
-var dbsetting = require('./dbsetting');
-var Db = require('mongodb').Db;
-var Connection = require('mongodb').connection;
-var Server = require('mongodb').server;
-
-module.exports = new Db(dbsetting.db,new Server(dbsetting.host, dbsetting.port),{safe: true});
--- expressl/myapp/config/dbsetting.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    cookieSecret : 'myblog',
-    db : 'mydb',
-    host: 'localhost',
-    port: '27017'
-};
