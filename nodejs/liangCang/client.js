08bb6f66567894b6a5b075de3696e1391a88a1f8
ios-android <uuooooq@qq.com>
2019-12-24 18:38:43 +0800

    socket

---
+++ nodejs/liangCang/client.js
@@ -0,0 +1,25 @@
+const net = require('net');
+
+const socket = new net.Socket();
+
+const port = 8000;
+
+const hostname = '127.0.0.1';
+
+socket.setEncoding = 'UTF-8';
+
+socket.connect( port,hostname,function(){
+  socket.write('hello 大家好~~');
+});
+
+socket.on( 'data', function ( msg ) {
+  console.log( msg );
+});
+
+socket.on( 'error', function ( error ) {
+  console.log( 'error' + error );
+});
+
+socket.on('close',function(){
+  console.log('服务器端下线了');
+});
